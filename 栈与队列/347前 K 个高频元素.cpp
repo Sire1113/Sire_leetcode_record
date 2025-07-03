aff2e602d8c6c0b6cbdae57e2817e2356e9dddd1
sire <Sire1113@outlook.com>
2025-07-03 13:50:45 +0800

    feat(sire 7.2): add 栈与队列 347. 前 K 个高频元素

---
+++ 栈与队列/347前 K 个高频元素.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        auto mycmp = [](pair<int, int>& left, pair<int, int>& right){
+            return left.second > right.second;
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(mycmp)> que(mycmp);
+        unordered_map<int, int> cnt;
+        vector<int> res(k);
+        for (auto& elem: nums)
+            cnt[elem]++;
+        for (auto& kv: cnt){
+            que.push(kv);
+            if (que.size()>k)
+                que.pop();
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = que.top().first;
+            que.pop();
+        }
+        return res;
+    }
+};
