c067f1d7e7af943178a91e82911f7237473de6cf
Sire1 <15964275549@163.com>
2025-08-26 10:25:07 +0800

    feat(sire 8.17):add 图论 695 岛屿的最大面积

---
+++ 图论/695岛屿的最大面积.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int bfs(vector<vector<int>>& grid, vector<vector<int>> visited, int x, int y) {
+        int area{1};
+        queue<pair<int, int>> q;
+        q.push({x, y});
+        array<array<int, 2>, 4> dir{0, 1, 1, 0, -1, 0, 0, -1};
+        while (!q.empty()) {
+            auto [cur_x, cur_y] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int next_x = cur_x + dir[i][0];
+                int next_y = cur_y + dir[i][1];
+                if (next_x < 0 || next_x >= grid.size() || next_y < 0 || next_y >= grid[0].size())
+                    continue;
+                if (grid[next_x][next_y] == 1 && visited[next_x][next_y] != 1) {
+                    visited[next_x][next_y] = 1;
+                    q.push({next_x, next_y});
+                    area++;
+                }
+            }
+        }
+        return area;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int max_area{};
+        vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size(), 0));
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1 && visited[i][j] != 1) {
+                    visited[i][j] = 1;
+                    max_area = max(max_area, bfs(grid, visited, i, j));
+                }
+            }
+        }
+        return max_area;
+    }
+};
